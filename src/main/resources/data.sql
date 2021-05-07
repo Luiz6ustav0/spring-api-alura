ec5f6caaf1c23467a747aa759502c15e39671e9f
Luiz Almeida <luizorst@hotmail.com>
2021-05-07 18:45:54 -0300

    [ADD] basic authentication working

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '1234321');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$lfsZUPlU4fh6A2tv5AcRy.Z.NVsjAFhsKFOqcR5fSnUOVuwv8dfUO');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML5', 'Front-end');
