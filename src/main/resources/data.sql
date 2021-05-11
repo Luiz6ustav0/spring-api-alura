2c53cc6ba61d653b13b6da2d34eab38b290e3953
Luiz Almeida <luizorst@hotmail.com>
2021-05-11 19:06:33 -0300

    [ADD] basic role configuration for DELETE on /topicos/{id}

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$lfsZUPlU4fh6A2tv5AcRy.Z.NVsjAFhsKFOqcR5fSnUOVuwv8dfUO');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Luizera', 'luiz@email.com', '$2a$10$lfsZUPlU4fh6A2tv5AcRy.Z.NVsjAFhsKFOqcR5fSnUOVuwv8dfUO');
+
+INSERT INTO PERFIL(id, nome) VALUES (1, 'ROLE_ALUNO');
+INSERT INTO PERFIL(id, nome) VALUES (2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES (1, 1);
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES (2, 2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML5', 'Front-end');
